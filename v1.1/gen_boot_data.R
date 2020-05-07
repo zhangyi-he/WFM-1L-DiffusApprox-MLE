9c85888fd3fd8986643171cfa6cbe458a8320eed
Zhangyi He <zhangyi-he@users.noreply.github.com>
2020-05-07 16:18:33 +0100

    Update gen_boot_data.R

--- v1.1/gen_boot_data.R
+++ v1.1/gen_boot_data.R
@@ -38,3 +38,34 @@ for (i_params in 1:1100) {
   file_name = sprintf('samples%d/sample%d.RData', i_base_dir, i_params)
   save(counts, params_data, file=file_name)
 }
+
+# generate bootstrap data, resample from the 65 time points
+i_base_dir = 40 # bootstrap samples for sample0
+
+if (i_base_dir == 40) {
+  load('samples0/sample0.RData')
+} else if (i_base_dir == 41) {
+  load('samples0/sample1.RData')
+}
+
+counts0 = counts
+params_data0 = params_data
+params_data = params_data0[2,,drop=FALSE]
+counts1 = counts[,,2]
+n_ts = dim(counts1)[2]
+
+for (i_params in 1:1100) {
+  if (i_params %% 100 == 0) cat(i_params, "\n")
+  bin_resampled = counts1[, sort(sample.int(n_ts, replace=TRUE))]
+  ts = unique(bin_resampled[1,])
+  counts = array(0, c(3, length(ts), 1))
+  counts[1, ,1] = ts
+  for (i in 1:length(ts)) {
+    js = which(bin_resampled[1,]==ts[i])
+    counts[2,i,1] = sum(bin_resampled[2,js])
+    counts[3,i,1] = sum(bin_resampled[3,js])
+  }
+  params_data[1,2] = length(ts)
+  file_name = sprintf('samples%d/sample%d.RData', i_base_dir, i_params)
+  save(counts, params_data, file=file_name)
+}
